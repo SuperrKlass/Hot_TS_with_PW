45e5430b07632ff40ca9eeee0eb7a446eb5398f7
Elina Benemianovych <elya.benia@gmail.com>
2025-08-20 20:47:24 +0300

    Update

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,8 +29,13 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    // trace: 'on-first-retry',
+    trace: 'on',
+    video: 'on',
+    screenshot: 'only-on-failure',
   },
+
+  outputDir: 'playwright-artifacts', 
 
   /* Configure projects for major browsers */
   projects: [
--- tests/contact-us.test.ts
+++ tests/contact-us.test.ts
@@ -2,20 +2,20 @@ import { test, expect } from '@playwright/test';
 
 const users = ['test75315611@test.com', 'abracadabrabro@test.com']
 
-for (const user of users) {
-  test('test' + user, async ({ page }) => {
-    await page.goto('https://mern-store-gold.vercel.app/contact');
-    await page.getByRole('textbox', { name: 'You Full Name' }).click();
-    await page.getByRole('textbox', { name: 'You Full Name' }).fill('test');
-    await page.getByRole('textbox', { name: 'Your Email Address' }).click();
-    await page.getByRole('textbox', { name: 'Your Email Address' }).fill(Date.now() + user);
-    await page.getByRole('textbox', { name: 'Please Describe Your Message' }).click();
-    await page.getByRole('textbox', { name: 'Please Describe Your Message' }).fill('Hello my first test by Playwright!');
-    await page.getByRole('button', { name: 'Submit' }).click();
+// for (const user of users) {
+//   test('test' + user, async ({ page }) => {
+//     await page.goto('https://mern-store-gold.vercel.app/contact');
+//     await page.getByRole('textbox', { name: 'You Full Name' }).click();
+//     await page.getByRole('textbox', { name: 'You Full Name' }).fill('test');
+//     await page.getByRole('textbox', { name: 'Your Email Address' }).click();
+//     await page.getByRole('textbox', { name: 'Your Email Address' }).fill(Date.now() + user);
+//     await page.getByRole('textbox', { name: 'Please Describe Your Message' }).click();
+//     await page.getByRole('textbox', { name: 'Please Describe Your Message' }).fill('Hello my first test by Playwright!');
+//     await page.getByRole('button', { name: 'Submit' }).click();
 
-    await expect(page.getByRole('textbox', { name: 'You Full Name' })).toBeEmpty();
-  });
-}
+//     await expect(page.getByRole('textbox', { name: 'You Full Name' })).toBeEmpty();
+//   });
+// }
 
 
 
